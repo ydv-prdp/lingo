a20f9c5332604bac934601b1ba36383bc4cb65b8
Pradeep Yadav <pradeepyadav101010@gmail.com>
2024-03-26 07:12:56 +0530

    2:marketing skeletion

---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,9 @@
+
+
+export const Header = () => {
+  return (
+    <div className="h-20 w-full border-b-2 border-slate-200 px-4">
+        Header
+    </div>
+  )
+}
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+  <div className="text-green-500 font-bold text-lg">
+    This is a marketing page
+  </div>
+  );
+}
