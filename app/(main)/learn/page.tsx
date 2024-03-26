64f2cc2fddec3802a2e9193b26530dc34eb61047
Pradeep Yadav <pradeepyadav101010@gmail.com>
2024-03-26 23:04:57 +0530

    8:learn page wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,8 +1,24 @@
+import { FeedWrapper } from "@/components/feedwrapper"
+import { StickyWrapper } from "@/components/sticky-wrapper"
+import { Header } from "./header"
+import { UserProgress } from "@/components/user-progress"
 
 
 const LearnPage = () => {
   return (
-    <div>LearnPage</div>
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{title:"Spanish", imageSrc:"/es.svg"}}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish"/>
+      </FeedWrapper>
+    </div>
   )
 }
 
